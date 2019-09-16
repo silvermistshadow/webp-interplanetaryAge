d6010f3ca3843bd84871b8b968df26f163fd1b53
silvermistshadow <bmead72@gmail.com>
2019-09-16 11:44:35 -0700

    add libraries and base files

---
+++ karma.conf.js
@@ -0,0 +1,38 @@
+// Karma configuration
+// Generated on Mon Aug 12 2019 14:09:31 GMT-0700 (Pacific Daylight Time)
+
+const webpackConfig = require('./webpack.config.js');
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jquery-3.3.1', 'jasmine'],
+    files: [
+      'src/*.js',
+      'spec/*spec.js'
+    ],
+    webpack: webpackConfig,
+    exclude: [
+    ],
+    preprocessors: {
+      'src/*.js': ['webpack', 'sourcemap'],
+      'spec/*spec.js': ['webpack', 'sourcemap']
+    },
+    plugins: [
+      'karma-jquery',
+      'karma-webpack',
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-jasmine-html-reporter',
+      'karma-sourcemap-loader'
+    ],
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
